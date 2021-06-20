08a1e61014cd7c11b442212b864f178981b755f8
wangli <1151723225@qq.com>
2021-06-20 15:21:17 +0800

    feat: 商品SPU规格维护-100

--- gulimall-product/src/main/java/com/wangli/gulimall/product/controller/AttrController.java
+++ gulimall-product/src/main/java/com/wangli/gulimall/product/controller/AttrController.java
@@ -1,17 +1,18 @@
 package com.wangli.gulimall.product.controller;
 
-import java.util.Arrays;
-import java.util.Map;
-
+import com.wangli.common.utils.PageUtils;
+import com.wangli.common.utils.R;
+import com.wangli.gulimall.product.entity.ProductAttrValueEntity;
+import com.wangli.gulimall.product.service.AttrService;
+import com.wangli.gulimall.product.service.ProductAttrValueService;
 import com.wangli.gulimall.product.vo.AttrRespVo;
 import com.wangli.gulimall.product.vo.AttrVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import com.wangli.gulimall.product.entity.AttrEntity;
-import com.wangli.gulimall.product.service.AttrService;
-import com.wangli.common.utils.PageUtils;
-import com.wangli.common.utils.R;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -26,6 +27,17 @@ import com.wangli.common.utils.R;
 public class AttrController {
     @Autowired
     private AttrService attrService;
+    @Autowired
+    ProductAttrValueService productAttrValueService;
+
+    // /product/attr/base/listforspu/{spuId}
+    @GetMapping("/base/listforspu/{spuId}")
+    public R baseAttrlistforspu(@PathVariable("spuId") Long spuId) {
+
+        List<ProductAttrValueEntity> entities = productAttrValueService.baseAttrlistforspu(spuId);
+
+        return R.ok().put("data", entities);
+    }
 
 
     //product/attr/sale/list/0?
@@ -81,6 +93,17 @@ public class AttrController {
 
         return R.ok();
     }
+
+    ///product/attr/update/{spuId}
+    @PostMapping("/update/{spuId}")
+    public R updateSpuAttr(@PathVariable("spuId") Long spuId,
+                           @RequestBody List<ProductAttrValueEntity> entities) {
+
+        productAttrValueService.updateSpuAttr(spuId, entities);
+
+        return R.ok();
+    }
+
 
     /**
      * 删除
