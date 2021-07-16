21785639d3fd5e927278a332650b192816102b5f
wangli <1151723225@qq.com>
2021-07-16 21:51:27 +0800

    feat: 初始化认证服务&添加静态资源

---
+++ gulimall-auth-server/src/test/java/com/wangli/gulimall/auth/GulimallAuthServerApplicationTests.java
@@ -0,0 +1,18 @@
+package com.wangli.gulimall.auth;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@Slf4j
+class GulimallAuthServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
