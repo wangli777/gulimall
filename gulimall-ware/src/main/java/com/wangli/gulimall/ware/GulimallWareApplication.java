47a3b2d9456c2e61eea8c50f70edb4ac432ae095
wangli <1151723225@qq.com>
2021-09-14 22:33:44 +0800

    feat: 订单服务-使用延时队列解锁库存-1

--- gulimall-ware/src/main/java/com/wangli/gulimall/ware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/wangli/gulimall/ware/GulimallWareApplication.java
@@ -1,10 +1,12 @@
 package com.wangli.gulimall.ware;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableRabbit
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.wangli.gulimall.ware.feign")
---
+++ gulimall-ware/src/main/java/com/wangli/gulimall/ware/config/MyRabbitConfig.java
@@ -0,0 +1,104 @@
+package com.wangli.gulimall.ware.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+
+/**
+ * @author WangLi
+ * @date 2021/9/14 22:17
+ * @description
+ */
+@Configuration
+public class MyRabbitConfig {
+    /**
+     * mq消息使用json序列化
+     *
+     * @return
+     */
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+     @RabbitListener(queues = "stock.release.stock.queue")
+     public void handle(Message message) {
+
+     }
+    /**
+     * 库存服务默认的交换机
+     * @return
+     */
+    @Bean
+    public Exchange stockEventExchange() {
+        //String name, boolean durable, boolean autoDelete, Map<String, Object> arguments
+        TopicExchange topicExchange = new TopicExchange("stock-event-exchange", true, false);
+        return topicExchange;
+    }
+
+    /**
+     * 普通队列
+     * @return
+     */
+    @Bean
+    public Queue stockReleaseStockQueue() {
+        //String name, boolean durable, boolean exclusive, boolean autoDelete, Map<String, Object> arguments
+        Queue queue = new Queue("stock.release.stock.queue", true, false, false);
+        return queue;
+    }
+
+
+    /**
+     * 延迟队列
+     * @return
+     */
+    @Bean
+    public Queue stockDelay() {
+
+        HashMap<String, Object> arguments = new HashMap<>();
+        arguments.put("x-dead-letter-exchange", "stock-event-exchange");
+        arguments.put("x-dead-letter-routing-key", "stock.release");
+        // 消息过期时间 2分钟
+        arguments.put("x-message-ttl", 120000);
+
+        Queue queue = new Queue("stock.delay.queue", true, false, false,arguments);
+        return queue;
+    }
+
+
+    /**
+     * 交换机与普通队列绑定
+     * @return
+     */
+    @Bean
+    public Binding stockLocked() {
+        //String destination, DestinationType destinationType, String exchange, String routingKey,
+        // 			Map<String, Object> arguments
+        Binding binding = new Binding("stock.release.stock.queue",
+                Binding.DestinationType.QUEUE,
+                "stock-event-exchange",
+                "stock.release.#",
+                null);
+
+        return binding;
+    }
+
+
+    /**
+     * 交换机与延迟队列绑定
+     * @return
+     */
+    @Bean
+    public Binding stockLockedBinding() {
+        return new Binding("stock.delay.queue",
+                Binding.DestinationType.QUEUE,
+                "stock-event-exchange",
+                "stock.locked",
+                null);
+    }
+}
